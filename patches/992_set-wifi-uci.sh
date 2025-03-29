f7e792e888c3b5144cfe480be9450390609ab5fb
ll1233 <68339285+fisher423@users.noreply.github.com>
2025-03-29 17:52:19 +0800

    Update 992_set-wifi-uci.sh
    Signed-off-by: ll1233 <68339285+fisher423@users.noreply.github.com>

--- patches/992_set-wifi-uci.sh
+++ patches/992_set-wifi-uci.sh
@@ -65,10 +65,6 @@ redmi_ax6_wifi_cfg() {
 }
 
 case "${board_name}" in
-jdcloud,ax1800-pro | \
-    jdcloud,re-ss-01)
-    jdc_ax1800_pro_wifi_cfg
-    ;;
 jdcloud,ax6600 | \
     jdcloud,re-cs-02)
     jdc_ax6600_wifi_cfg
